baacb104be41d9858099eee75c78cfaddfbd52b4
mohamed-a95 <mohamedabdihakima95@gmail.com>
2024-06-14 01:14:06 +0200

    adderat knappar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 function App() {
-  return <div className="container"></div>;
+  return (
+    <div className="container">
+      <div className="star"></div>
+      <div className="header"></div>
+      <div className="paragraph"></div>
+
+      <ul className="list-btn">
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </ul>
+    </div>
+  );
 }
 
 export default App;
