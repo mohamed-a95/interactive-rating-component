8c7f96871d2fa6b2305c84a1e18ead4f1669797c
mohamed-a95 <mohamedabdihakima95@gmail.com>
2024-06-15 00:40:15 +0200

    webappen är klar, har samtliga funktioner och även en return knapp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
 import { useState } from "react";
+import img1 from "./Images/icon-star.svg";
+import img2 from "./Images/illustration-thank-you.svg";
 
 function App() {
-  const [value, setValue] = useState(0);
-  const handleClick = (arg) => {
-    setValue(arg);
-    console.log(value);
-  };
-  return (
-    <div className="container">
-      <div className="star"></div>
-      <div className="header"></div>
-      <div className="paragraph"></div>
+  const [selected, setSelected] = useState(false);
+  const [ranking, setRanking] = useState(0);
+  const [issubmitted, setIsSubmitted] = useState(true);
 
-      <ul className="list-btn">
-        <li onClick={() => handleClick(1)}>1</li>
-        <li onClick={() => handleClick(2)}>2</li>
-        <li onClick={() => handleClick(3)}>3</li>
-        <li onClick={() => handleClick(4)}>4</li>
-        <li onClick={() => handleClick(5)}>5</li>
-      </ul>
-    </div>
+  const handleClick = (arg) => {
+    setSelected(arg);
+    setRanking(arg);
+  };
+
+  const handleSubmit = () => {
+    setIsSubmitted(true);
+  };
+
+  const handleReturn = () => {
+    setIsSubmitted(false);
+  };
+
+  return (
+    <>
+      <div className={!issubmitted ? "container" : "container-hide"}>
+        <div className="star">
+          <img src={img1} alt="Star Icon" />
+        </div>
+        <div className="header">How did we do?</div>
+        <div className="paragraph">
+          Please let us know how we did with your support request. All feedback
+          is appreciated to help us improve our offering!
+        </div>
+
+        <ul className="list-btn">
+          <li
+            className={selected === 1 ? "selected" : "listknapp"}
+            onClick={() => handleClick(1)}
+          >
+            1
+          </li>
+          <li
+            className={selected === 2 ? "selected" : "listknapp"}
+            onClick={() => handleClick(2)}
+          >
+            2
+          </li>
+          <li
+            className={selected === 3 ? "selected" : "listknapp"}
+            onClick={() => handleClick(3)}
+          >
+            3
+          </li>
+
+          <li
+            className={selected === 4 ? "selected" : "listknapp"}
+            onClick={() => handleClick(4)}
+          >
+            4
+          </li>
+          <li
+            className={selected === 5 ? "selected" : "listknapp"}
+            onClick={() => handleClick(5)}
+          >
+            5
+          </li>
+        </ul>
+        <div className="btn">
+          <button className="btn-submit" onClick={handleSubmit}>
+            sumbit
+          </button>
+        </div>
+      </div>
+
+      <div className={issubmitted ? "container2" : "container-hide"}>
+        <div className="tel-img">
+          <img src={img2} alt="thankyouimage"></img>
+        </div>
+        <div className="small-text">You selected {ranking} out of 5 </div>
+        <div className="header2">Thank you!</div>
+        <div className="paragraph2">
+          {" "}
+          We appreciate you taking the time to give a rating. If you ever need
+          more support, don’t hesitate to get in touch!
+        </div>
+        <div className="btn">
+          <button className="btn-return" onClick={handleReturn}>
+            return
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
 
